f6fbaf3c1f4b74418aabe7b47e36af43e8080590
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-05-14 02:09:23 +0900

    feat: Create a character on top of a circle

--- Assets/ARController.cs
+++ Assets/ARController.cs
@@ -6,56 +6,50 @@ using UnityEngine.UI;
 public class ARController : MonoBehaviour
 {
     public GameObject characterPrefab; // 캐릭터 프리팹
-    public AudioClip musicClip; // 캐릭터 생성 시 재생할 노래 클립
-    public GameObject messageText; //알림 Text 객체
     public ARRaycastManager arRaycastManager; // AR 레이캐스트 매니저
-
-    private List<ARRaycastHit> hitResults = new List<ARRaycastHit>(); // 레이캐스트 히트 결과를 저장할 리스트
-    private float minScale = 0.5f; // 캐릭터의 최소 크기
-    private float maxScale = 1.5f; // 캐릭터의 최대 크기
-    private bool isPlacingObject = false; // 캐릭터 생성 중인지 여부
+    public GameObject alarm;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>(); // 레이캐스트 히트 결과를 저장할 리스트
+    private void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
     void Update()
     {
-        if (!isPlacingObject) return; // 캐릭터 생성 중이 아니면 종료
-
-        if (arRaycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hitResults, TrackableType.PlaneWithinPolygon))
+        //터치가 없으면 실행 X
+        if (Input.touchCount == 0) return;
+        if (arRaycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
         {
-            //화면 중앙 평면 인식
-            Pose hitPose = hitResults[0].pose;
-            ARPlane plane = hitResults[0].trackable as ARPlane;
-            if (plane != null)
+            //터치 한 곳에서 생성
+            Pose hitPose = hits[0].pose;
+
+            // 원의 중심점과 반지름
+            Vector3 center = new Vector3(0f, 0f, 0f); // 중심점 좌표
+            float radius = 0.5f; // 반지름
+
+            if (Vector3.Distance(center, hitPose.position) < radius)
             {
-                GenerateCharacter(hitPose, plane);
+                //캐릭터 생성
+                GameObject characterObject = Instantiate(characterPrefab, hitPose.position, hitPose.rotation);
+
+                // 랜덤한 방향으로 캐릭터 회전
+                float randomRotationY = Random.Range(0f, 360f);
+                Quaternion randomRotation = Quaternion.Euler(0f, randomRotationY, 0f);
+                characterObject.transform.rotation = randomRotation;
+
+                // 캐릭터 생성 후, 원의 크기에 따라 캐릭터의 크기 조절
+                float scale = Random.Range(0.05f, 0.1f);
+                characterObject.transform.localScale = Vector3.one * scale;
+
+                alarm.GetComponent<Text>().text = "";
+
+                // 생성된 캐릭터에 AudioSource 컴포넌트를 추가하고 노래를 재생
+                GameManager.instance.PlaySoundOnInstantiate(characterObject);
             }
-            else
-            {
-                // 평면 인식 실패 시 메시지 출력
-                messageText.GetComponent<Text>().text = "Where are you?";
-            }
+            else { alarm.GetComponent<Text>().text = "Where is klee?"; }
         }
     }
-    public GameObject GenerateCharacter(Pose hitPose, ARPlane plane)
+    void OnDisable()
     {
-        //캐릭터 생성
-        GameObject characterObject = Instantiate(characterPrefab, hitPose.position, hitPose.rotation);
-
-        // 랜덤한 방향으로 캐릭터 회전
-        float randomRotationY = Random.Range(0f, 360f);
-        Quaternion randomRotation = Quaternion.Euler(0f, randomRotationY, 0f);
-        characterObject.transform.rotation = randomRotation;
-
-        // 캐릭터 생성 후, 원의 크기에 따라 캐릭터의 크기 조절
-        float scale = Mathf.Clamp(plane.size.x, minScale, maxScale);
-        characterObject.transform.localScale = Vector3.one * scale;
-
-        // 생성된 캐릭터에 AudioSource 컴포넌트를 추가하고 노래를 재생
-        AudioSource audioSource = characterObject.AddComponent<AudioSource>();
-        audioSource.clip = musicClip;
-        audioSource.Play();
-
-        isPlacingObject = true; // 캐릭터 생성 완료
-        messageText.GetComponent<Text>().text = "";
-
-        return characterObject;
+        hits.Clear();
     }
 }
