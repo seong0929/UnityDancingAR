e26c088964bc3cf15ef0c6b4e784d37c4eb89f12
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-05-17 14:03:24 +0900

    feat: Complete without sound

--- Assets/ARController.cs
+++ Assets/ARController.cs
@@ -8,10 +8,12 @@ public class ARController : MonoBehaviour
 {
     public GameObject characterPrefab; // 캐릭터 프리팹
     public ARRaycastManager arRaycastManager; // AR 레이캐스트 매니저
-    public GameObject alarm;
+    public GameObject alarm;    // 알람 문구
     public int maxCharacterCount = 5; // 최대 캐릭터 수
+
     private List<ARRaycastHit> hits = new List<ARRaycastHit>(); // 레이캐스트 히트 결과를 저장할 리스트
-    private int currentCharacterCount = 0; // 현재 캐릭터 수
+    private List<GameObject> characters = new List<GameObject>(); // 생성된 캐릭터를 저장할 리스트
+    private bool isTouchInProgress = false; // 터치 진행 중 여부
 
     private void Awake()
     {
@@ -21,10 +23,13 @@ public class ARController : MonoBehaviour
     void Update()
     {
         // 터치가 없거나 최대 캐릭터 수를 초과하면 실행하지 않음
-        if (Input.touchCount == 0 || currentCharacterCount >= maxCharacterCount) return;
+        if (Input.touchCount == 0 || characters.Count >= maxCharacterCount) return;
 
         if (arRaycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
         {
+            // 중복 터치 입력 방지
+            isTouchInProgress = true;
+
             // 터치 한 곳에서 생성
             Pose hitPose = hits[0].pose;
 
@@ -42,7 +47,7 @@ public class ARController : MonoBehaviour
                 Quaternion randomRotation = Quaternion.Euler(0f, randomRotationY, 0f);
                 characterObject.transform.rotation = randomRotation;
 
-                // 캐릭터 생성 후, 원의 크기에 따라 캐릭터의 크기 조절
+                // 캐릭터 생성 후, 랜덤하게 캐릭터의 크기 조절
                 float scale = Random.Range(0.05f, 0.1f);
                 characterObject.transform.localScale = Vector3.one * scale;
 
@@ -51,19 +56,37 @@ public class ARController : MonoBehaviour
                 // 생성된 캐릭터에 AudioSource 컴포넌트를 추가하고 노래를 재생
                 GameManager.instance.PlaySoundOnInstantiate(characterObject);
 
-                // 캐릭터 수 증가
-                currentCharacterCount++;
+                // 캐릭터를 리스트에 추가
+                characters.Add(characterObject);
+
+                // 캐릭터 수가 최대 캐릭터 수를 초과하는 경우 가장 오래된 캐릭터를 삭제
+                if (characters.Count > maxCharacterCount)
+                {
+                    GameObject oldestCharacter = characters[0];
+                    characters.RemoveAt(0);
+                    Destroy(oldestCharacter);
+                }
             }
             else
             {
-                alarm.GetComponent<Text>().text = "Where is klee?";
+                alarm.GetComponent<Text>().text = "Where is Klee?";
             }
         }
     }
-
     void OnDisable()
     {
-        hits.Clear();
-        currentCharacterCount = 0; // 캐릭터 수 초기화
+        // 생성된 모든 캐릭터를 삭제
+        foreach (GameObject character in characters)
+        {
+            Destroy(character);
+        }
+
+        // 리스트 초기화
+        characters.Clear();
+    }
+    // 캐릭터 생성 후 삭제하는 부분
+    void OnDestroy()
+    {
+        isTouchInProgress = false; // 터치 상태 초기화
     }
 }
